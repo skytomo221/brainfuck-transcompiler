b983afff3bfd78446ea0404ca8c34e8646f431d9
skytomo <tomo1kawa.fullerene@gmail.com>
2022-07-29 13:15:08 +0000

    First commit

---
+++ spec/brainfuck_transcompiler_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe BrainfuckTranscompiler do
+  it "has a version number" do
+    expect(BrainfuckTranscompiler::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
